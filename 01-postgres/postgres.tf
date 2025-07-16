d5fbe8142a0ff626c4aa3f5949936b5679231ada
Mike Monaco <mamonaco1973@gmail.com>
2025-07-16 16:27:36 -0400

    v1 of postgres server

---
+++ 01-postgres/postgres.tf
@@ -0,0 +1,32 @@
+resource "google_sql_database_instance" "postgres" {
+  name             = "postgres-instance"
+  database_version = "POSTGRES_15"
+  region        = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.project_vpc.self_link
+    }
+
+    backup_configuration {
+      enabled = true
+    }
+
+    maintenance_window {
+      day          = 7
+      hour         = 3
+      update_track = "stable"
+    }
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_user" "postgres_user" {
+  name     = "postgres"
+  instance = google_sql_database_instance.postgres.name
+  password = random_password.postgres.result
+}
